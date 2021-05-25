68842c4c68b53fe1c37568cdc169582b20aebede
tarun13rathore <tarun13rathore@gmail.com>
2021-05-25 11:02:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState,useEffect } from 'react';
 
 function App() {
+  const [widthCount,setWidthCount] = useState(window.screen.width);
+
+  const actualWidth =() =>{
+    setWidthCount(window.innerWidth);
+  }
+  useEffect(() => {
+    window.addEventListener('resize',actualWidth);
+    return () => {
+      window.removeEventListener('resize',actualWidth);
+    }
+  })
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>The Actual size of window is :</p>
+      <h1>{widthCount}</h1>
     </div>
   );
 }
